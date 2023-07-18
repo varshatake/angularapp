4f2522817f2fee71dca2ebb41034512297e87d35
varshatake <varshagore108@gmail.com>
2023-07-18 11:40:49 +0530

    *ngfor directive class session

---
+++ src/app/ngfor/ngfor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngfor',
+  templateUrl: './ngfor.component.html',
+  styleUrls: ['./ngfor.component.css']
+})
+export class NgforComponent implements OnInit {
+
+  employeeRecords : any [] = [
+    {eName:'Dnyanesh', eCity: 'pune', Salary: '150000' , imgUrl: 'https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg'},
+    {eName:'Jack', eCity: 'USA', Salary: '1500000', imgUrl: 'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg'},
+
+   ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
