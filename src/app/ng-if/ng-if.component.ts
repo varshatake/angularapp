32a493d3611cd99aae0abcc4326eb4012ce4e521
varshatake <varshagore108@gmail.com>
2023-07-18 16:40:59 +0530

    ngIf directive

--- src/app/ng-if/ng-if.component.ts
+++ src/app/ng-if/ng-if.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgIfComponent implements OnInit {
 
+  show:boolean =true;
+
   showActions: boolean = true;
   constructor() { }
 
