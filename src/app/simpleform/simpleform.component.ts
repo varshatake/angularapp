b2010bf97883592d3ec6caf5dd11beb0acb03b14
varshatake <varshagore108@gmail.com>
2023-07-25 23:25:57 +0530

    simple form class file

---
+++ src/app/simpleform/simpleform.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-simpleform',
+  templateUrl: './simpleform.component.html',
+  styleUrls: ['./simpleform.component.css']
+})
+export class SimpleformComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  OnSubmit(form: NgForm) {
+
+    console.log('form submitted', form.value);
+
+  }
+
+
+}
