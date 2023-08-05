3ed725a1bbcc2c915c1ec2a7603f3b83e15a30d7
varshatake <varshagore108@gmail.com>
2023-08-05 18:18:29 +0530

    simple template form class fille

--- src/app/simpleform/simpleform.component.ts
+++ src/app/simpleform/simpleform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-simpleform',
@@ -8,7 +8,28 @@ import { NgForm } from '@angular/forms';
 })
 export class SimpleformComponent implements OnInit {
 
-  
+  isFormSubmitted = false;
+  username = '';
+  defaultCourseValue = 'JavaScript';
+  colorIs : 'azure';
+
+  isEmailValid : boolean = false;
+  isEmail : boolean; 
+
+  genders = [
+    { id: '1', value: 'Male'},
+    { id: '2', value: 'Female'},
+    { id: '3', value: 'Other'},
+  ]
+
+  formData = {
+    username: '',
+    email: '',
+    course: '',
+    country: '',
+    state: '',
+    gender:''
+   }
   constructor() { }
 
   ngOnInit() {
@@ -16,9 +37,59 @@ export class SimpleformComponent implements OnInit {
 
   OnSubmit(form: NgForm) {
 
+    this.isFormSubmitted = true;
     console.log('form submitted', form.value);
+   
+    let email = form.value.email;
 
+    console.log('Email is', email);
+
+    let country = form.value.address.country;
+
+    console.log('user input country name is', country)
+
+    this.formData.username = form.value.username;
+    this.formData.email = form.value.email;
+    this.formData.course = form.value.course;
+    this.formData.country = form.value.address.country;
+    this.formData.state = form.value.address.state;
+    this.formData.gender = form.value.gender;
+    // reser the form controls
+    
+    form.reset();
+    //form.resetForm({course: 'Angular'});
+    form.controls['course'].setValue(this.defaultCourseValue);
+    //form.controls['course'].patchValue(this.defaultCourseValue);
+    form.controls['email'].setValue('shaurya@gmail.com');    
+    
+    // to set default value for nested group
+  //   console.log(form.controls['address']); 
+  //   let objaddress = form.controls['address'];
+
+  //   console.log('after getting address form group', objaddress);
+  //  objaddress.controls['country'].patchValue('India');        
   }
 
+  // resetform(form : NgForm) {
+  //   form.resetForm({
+  //     course : 'Javascript',
+  //   });
+  // }
 
+  checkEmail(email) {
+    console.log(email.target.value);
+
+    const domain = email.target.value.substring(email.target.value.lastIndexOf('@') + 1);
+    console.log('domain ',domain);
+
+    if(domain == 'codemindtechnology.com') 
+    {
+      // false
+      this.isEmailValid = false;
+    }
+    else {
+      // true
+      this.isEmailValid = true;
+    }
+  }
 }
