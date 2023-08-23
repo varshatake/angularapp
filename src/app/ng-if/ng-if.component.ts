ac79c25f978012b4c1cb40cead03efb35b842457
varshatake <varshagore108@gmail.com>
2023-08-23 09:26:49 +0530

    ngif updated with injected service

--- src/app/ng-if/ng-if.component.ts
+++ src/app/ng-if/ng-if.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Service1Service } from 'src/Services/service1.service';
 
 @Component({
   selector: 'app-ng-if',
   templateUrl: './ng-if.component.html',
-  styleUrls: ['./ng-if.component.css']
+  styleUrls: ['./ng-if.component.css'],
+  providers : [Service1Service]
 })
 export class NgIfComponent implements OnInit {
 
   show:boolean =true;
 
   showActions: boolean = true;
-  constructor() { }
+  constructor(private objservice : Service1Service) { }
 
   ngOnInit() {
+    this.objservice.hello;
   }
 
 }
