91343b957e7f07cd1bfe500ed257f7cf17f208ff
varshatake <varshagore108@gmail.com>
2023-08-23 09:27:16 +0530

    Custom created pipe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, searchTearm: any): any {
+    return value.filter(function(search) {
+      return search.productName.toLowerCase().indexOf(searchTearm.toLowerCase()) > -1
+    })
+  }
+
+}
